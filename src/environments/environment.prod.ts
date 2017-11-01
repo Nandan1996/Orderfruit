91d806eaef3f3d1ce9ae8cf3c003fc5d699a2c3d
Nandan1996 <choudharynandan260@gmail.com>
2017-11-01 12:12:19 +0530

    Add navigation bar & define routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLpwRRMJo7HBMnLxMg_-xIQPn9ygCxziU',
+    authDomain: 'orderfruit-5.firebaseapp.com',
+    databaseURL: 'https://orderfruit-5.firebaseio.com',
+    projectId: 'orderfruit-5',
+    storageBucket: '',
+    messagingSenderId: '706573773534'
+  }
 };
