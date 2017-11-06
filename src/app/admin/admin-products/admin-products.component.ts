e3b29dea8e074a9783054f07d177650020f00a2b
Nandan1996 <choudharynandan260@gmail.com>
2017-11-06 14:40:01 +0530

    Add a search bar in admin product page.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,38 @@
+import { Product } from './../../models/product';
 import { Observable } from 'rxjs/Observable';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$: Observable<any[]>;
+  subscription: Subscription;
+  products: Product[];
+  filteredProducts: Product[];
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.products = this.filteredProducts = products);
   }
 
+  filter(query: string) {
+    if (query) {
+      this.filteredProducts = this.products
+        .filter(product => product.title.toLowerCase()
+                            .includes(query.toLowerCase()));
+    }else {
+      this.filteredProducts = this.products;
+    }
+  }
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
