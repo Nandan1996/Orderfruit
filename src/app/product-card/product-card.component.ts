d2e85f4a1bc0f18b4470a0a8c921e25aff4d5508
Nandan1996 <choudharynandan260@gmail.com>
2017-11-07 15:58:24 +0530

    Assign a cart to each user.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from './../shopping-cart.service';
 import { Product } from './../models/product';
 import { Component, Input } from '@angular/core';
 
@@ -9,5 +10,17 @@ import { Component, Input } from '@angular/core';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
+
+  addToCart(product: Product) {
+    let cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      this.cartService.create().then(result => {
+        cartId = result.key;
+        localStorage.setItem('cartId', cartId);
+      });
+    }else {
+      // add product to cart
+    }
+  }
 }
