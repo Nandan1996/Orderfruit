00f5ab1c1dd43ccfda82e50086c0a5144c9084bd
Nandan1996 <choudharynandan260@gmail.com>
2017-11-13 12:55:15 +0530

    Add shipping form and store the order.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { OrderService } from './../order.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCart } from './../models/shopping-cart';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
+  constructor(
+    private cartService: ShoppingCartService,
+    private orderService: OrderService) { }
 
-  constructor() { }
+  async ngOnInit() {
+    const cart$ = await this.cartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart);
+  }
 
-  ngOnInit() {
+  placeOrder() {
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(item => {
+        return {
+          product: {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            price: item.price
+          },
+          quantity: item.quantity,
+          totalPrice: item.totalPrice
+        };
+      })
+    };
+    this.orderService.storeOrder(order);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
